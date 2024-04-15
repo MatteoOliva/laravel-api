875ebc4f7f93b47be89609444301d9cba2ab8a59
MatteoOliva <oliva.matteo98@outlook.com>
2024-04-15 17:00:53 +0200

    feat: formato il controller che risponde alle api e stampato il json

---
+++ app/Http/Controllers/Api/ProjectController.php
@@ -0,0 +1,37 @@
+<?php
+
+namespace App\Http\Controllers\Api;
+
+use App\Http\Controllers\Controller;
+use App\Models\Project;
+use Illuminate\Http\Request;
+
+
+class ProjectController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        $projects = Project::select('id','type_id','title','content','image')->with('type')->paginate();
+        return response()->json($projects);
+    }
+
+
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function show($id)
+    {
+        //
+    }
+
+  
+}
